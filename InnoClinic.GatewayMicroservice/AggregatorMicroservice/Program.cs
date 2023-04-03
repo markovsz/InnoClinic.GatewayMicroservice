23ab0358be7413570f01fd5efba8c5624dd66f91
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-03 12:22:01 +0300

    add basic project structure

---
+++ InnoClinic.GatewayMicroservice/AggregatorMicroservice/Program.cs
@@ -0,0 +1,29 @@
+using AggregatorMicroservice;
+using AggregatorMicroservice.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddHttpClient();
+builder.Services.ConfigureServices();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
