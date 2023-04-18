ed5ffcf8205d4331167922dc21694416a5747f64
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-18 12:53:59 +0300

    add serivces to di

---
+++ InnoClinic.GatewayMicroservice/AggregatorMicroservice/Extensions/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AggregatorMicroservice.Services;
+using AggregatorMicroservice.Services.Abstractions;
+
+namespace AggregatorMicroservice.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IHttpCrudClient, HttpCrudClient>();
+        services.AddScoped<IAggregatorsService, AggregatorsService>();
+    }
+}
