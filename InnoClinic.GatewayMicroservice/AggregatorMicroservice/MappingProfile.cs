d17dd298ddd17440dc2682ff4a0b3f928ceffb6a
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-18 12:54:48 +0300

    add mapping profile

---
+++ InnoClinic.GatewayMicroservice/AggregatorMicroservice/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AggregatorMicroservice.Models.DTOs.Aggregated;
+using AggregatorMicroservice.Models.DTOs.Outgoing;
+using AutoMapper;
+
+namespace AggregatorMicroservice;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<AppointmentByReceptionistOutgoingDto, AppointmentByReceptionistAggregatedDto>();
+		CreateMap<AppointmentScheduleByDoctorOutgoingDto, AppointmentScheduleByDoctorAggregatedDto>();
+		CreateMap<ResultOutgoingDto, ResultAggregatedDto>();
+		CreateMap<AppointmentForResultOutgoingDto, AppointmentForResultAggregatedDto>();
+	}
+}
