ef0a793e1161046cf87ea6b782c1f029dd7fac74
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-18 12:53:16 +0300

    add request parameters

---
+++ InnoClinic.GatewayMicroservice/AggregatorMicroservice/Models/RequestParameters/ScheduleParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace AggregatorMicroservice.Models.RequestParameters;
+
+public class ScheduleParameters
+{
+    public Guid PatientId { get; set; }
+    public int? Day { get; set; }
+    public int? Month { get; set; }
+    public int? Year { get; set; }
+}
