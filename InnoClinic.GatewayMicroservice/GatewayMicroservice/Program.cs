2cb63fcd9819db25da0c62f2f7e3d8248873666a
markovsz <58207528+markovsz@users.noreply.github.com>
2023-05-04 12:33:43 +0300

    remove swagger + build configuration

--- InnoClinic.GatewayMicroservice/GatewayMicroservice/Program.cs
+++ InnoClinic.GatewayMicroservice/GatewayMicroservice/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Configuration
-    .AddJsonFile("ocelot.json", false, true);
+    .AddJsonFile("ocelot.json", false, true)
+    .Build();
 
 builder.Services.AddOcelot(builder.Configuration);
 
@@ -15,8 +16,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseOcelot().Wait();
