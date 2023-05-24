e23b17e894742b5ea60335987e09502313ab59d6
markovsz <58207528+markovsz@users.noreply.github.com>
2023-05-24 13:01:40 +0300

    update exceptions handler + add new exceptions

---
+++ InnoClinic.GatewayMicroservice/AggregatorMicroservice/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace AggregatorMicroservice.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
