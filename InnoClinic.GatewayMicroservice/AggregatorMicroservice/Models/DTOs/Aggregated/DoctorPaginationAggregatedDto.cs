6ddc7f156721927035e2f1e805c77265bd921a55
markovsz <58207528+markovsz@users.noreply.github.com>
2023-08-02 23:40:31 +0300

    add CreatePatientByAdmin and DoctorPagination AggregatedDtos

---
+++ InnoClinic.GatewayMicroservice/AggregatorMicroservice/Models/DTOs/Aggregated/DoctorPaginationAggregatedDto.cs
@@ -0,0 +1,7 @@
+﻿namespace AggregatorMicroservice.Models.DTOs.Aggregated;
+
+public class DoctorPaginationAggregatedDto
+{
+    public IEnumerable<DoctorMinProfileAggregatedDto> Entities { get; set; }
+    public int PagesCount { get; set; }
+}
